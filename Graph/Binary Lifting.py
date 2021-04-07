4aa2d2db940ce83e4bf946469cf813e139dcbc80
carlos <carloscerlira@gmail.com>
2021-04-07 03:14:25 -0500

    added binary lifting

---
+++ Graph/Binary Lifting.py
@@ -0,0 +1,21 @@
+class TreeAncestor:
+    step = 16
+    def __init__(self, n: int, A: List[int]):
+        A = dict(enumerate(A))
+        jump = [A]
+        for s in range(self.step):
+            B = {}
+            for i in A:
+                if A[i] in A:
+                    B[i] = A[A[i]]
+            jump.append(B)
+            A = B
+        self.jump = jump
+
+    def getKthAncestor(self, node: int, k: int) -> int:
+        step = self.step
+        for i in range(step):
+            if k & (1 << i):
+                node = self.jump[i].get(node, -1)
+        return node
+
