0fba1884b3aeb665ad3af9fc177d9e96fcec581f
carlos <carloscerlira@gmail.com>
2021-04-08 19:42:48 -0500

    added topoligal sort

--- Chinese Remainder.py
+++ Chinese Remainder.py
@@ -7,8 +7,6 @@ def chinese_remainder(n, a):
         sum += a_i * mul_inv(p, n_i) * p
     return sum % prod
  
- 
- 
 def mul_inv(a, b):
     b0 = b
     x0, x1 = 0, 1
@@ -19,7 +17,6 @@ def mul_inv(a, b):
         x0, x1 = x1 - q * x0, x0
     if x1 < 0: x1 += b0
     return x1
- 
  
 n = [3, 5, 7]
 a = [2, 3, 2]
--- Graph/Topological Sort.py
+++ Graph/Topological Sort.py
@@ -1,3 +1,26 @@
-def solve():
-    return 
+from collections import defaultdict, deque
+
+#Time O(V+E)
+def solve(adj):
+    n = len(adj)
+    indegree = defaultdict(int)
+    for u in range(n):
+        for v in adj[u]:
+            indegree[v] += 1
+
+    queue = deque([])
+    for u in range(n):
+        if indegree[u] == 0:
+            queue.append(u)
+            
+    while queue:
+        u = queue.popleft()
+        for v in adj[u]:
+            indegree[v] -= 1
+            if indegree[v] == 0:
+                queue.append(v)
+    
+    for u in range(n):
+        if indegree[u] != 0: return False 
+    return True
 
