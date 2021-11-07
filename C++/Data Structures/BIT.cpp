bf2a992eb4cc9b2bbf62de9993fd850ba0d82f23
carlos <carloscerlira@gmail.com>
2021-11-07 09:30:24 -0600

    updated templates

--- C++/Data Structures/BIT.cpp
+++ C++/Data Structures/BIT.cpp
@@ -5,24 +5,45 @@ const long long MOD = 1e9 + 7;
 const long long INF = 1e9;
 const int N = 1e5+1;
 
-vector<long long> BIT(4*N, 0);
-
 int lsb(int pos) {
     return pos & -pos;
 }
 
-void update(int pos, int val){
-    while (pos <= N){
-        BIT[pos] += val;
-        pos += lsb(pos);
-    }
-}
+template<typename T>
+struct FenwickTree{
+    int n;
+    vector<T> BIT;
 
-int query(int pos){
-   int sum = 0;
-   while (pos > 0){
-      sum += BIT[pos];
-      pos -= lsb(pos);
-   }
-   return sum;
+    FenwickTree(int n){
+        this->n = n;
+        BIT.resize(4*n, 0);
+    }
+
+    void update(int pos, T val){
+        while (pos <= n){
+            BIT[pos] += val;
+            pos += lsb(pos);
+        }
+    }
+
+    T query(int pos){
+        T sum = 0;
+        while (pos > 0){
+            sum += BIT[pos];
+            pos -= lsb(pos);
+        }
+        return sum;
+    }
+};
+
+int main(){
+    vector<int> arr = {1,2,3,6};
+    int n = arr.size();
+    FenwickTree<int> BIT = FenwickTree<int>(n);
+    for(int i=1; i <= n; i++){
+        BIT.update(i, arr[i-1]);
+    }
+    int res = BIT.query(n);
+    cout << res << endl;
+    return 0;
 }
--- C++/Data Structures/Segment Tree.cpp
+++ C++/Data Structures/Segment Tree.cpp
@@ -1,40 +1,8 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-#define endl                        '\n'
-#define fastIO()                    cin.tie(0); cout.tie(0);
-#define FO(i, b)                    for (int i = 0; i < (b); i++)
-#define FOR(i, a, b)                for (int i = (a); i < (b); i++)
-#define rFOR(i, a, b)               for (int i = (a); i > (b); i--)
-#define TR(v, arr)                  for (auto& (v) : (arr))
-#define debug(x)                    cout << #x << " = "; _debug(x); cout << endl;
-#define si(x)	                    scanf("%d",&x)
-#define sl(x)	                    scanf("%lld",&x)
-#define ss(s)	                    scanf("%s",s)
-#define pi(x)	                    printf("%d\n",x)
-#define pl(x)	                    printf("%lld\n",x)
-#define ps(s)	                    printf("%s\n",s)
-#define pb                          push_back
-#define mp                          make_pair
-#define F                           first
-#define S                           second
-#define all(x)                      x.begin(), x.end() 
-#define sz(x)                       (int) x.size()
-#define LB(arr, x)                  lower_bound(all(arr), x)-(arr).begin()
-#define UB(arr, x)                  upper_bound(all(arr), x)-(arr).begin()
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
+const long long MOD = 1e9 + 7;
+const long long INF = 1e9;
 
 template<typename T>
 struct SegmentTree{
@@ -66,7 +34,7 @@ struct SegmentTree{
         return f(query(v*2, tl, tm, l, min(r, tm)), query(v*2+1, tm+1, tr, max(l, tm+1), r));
     }
  
-    void update(int v, int tl, int tr, int pos, int new_val) {
+    void update(int v, int tl, int tr, int pos, T new_val) {
         if (tl == tr){t[v] = new_val;} 
         else{
             int tm = tl+(tr-tl)/2;
@@ -81,15 +49,17 @@ struct SegmentTree{
     }
 };
 
+
+template<typename T>
 struct RangeUpdate {
-    vll t;
+    vector<T> t;
     
     RangeUpdate(int n){
         t.assign(4*n, 0);
         return;
     }
  
-    void build(vll& a, int v, int tl, int tr) {
+    void build(vector<T>& a, int v, int tl, int tr) {
         if (tl == tr) {
             t[v] = a[tl];
         } else {
@@ -100,19 +70,17 @@ struct RangeUpdate {
         }
     }
 
-    void update(int v, int tl, int tr, int l, int r, ll add) {
-        if (l > r)
-            return;
-        if (l == tl && r == tr) {
-            t[v] += add;
-        } else {
+    void update(int v, int tl, int tr, int l, int r, T add) {
+        if (l > r) return;
+        if (l == tl && r == tr) {t[v] += add;} 
+        else {
             int tm = (tl + tr) / 2;
             update(v*2, tl, tm, l, min(r, tm), add);
             update(v*2+1, tm+1, tr, max(l, tm+1), r, add);
         }
     }
 
-    ll get(int v, int tl, int tr, int pos) {
+    T get(int v, int tl, int tr, int pos) {
         if (tl == tr)
             return t[v];
         int tm = (tl + tr) / 2;
@@ -125,5 +93,11 @@ struct RangeUpdate {
  
 int main()
 {
+    vector<int> arr= {3,2,1};
+    int n = arr.size();
+    SegmentTree<int> st = SegmentTree<int>(n);
+    st.build(arr, 1, 0, n-1);
+    int res = st.query(1, 0, n-1, 0, n-1);
+    cout << res;
     return 0;
 }
